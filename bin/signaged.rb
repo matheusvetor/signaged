950f7e6d94b7eca96df016fa121efb4ceccce729
Felipe Oliveira Carvalho <felipekde@gmail.com>
2014-09-27 00:34:10 -0300

    Cleanup synchronizer and move instantiation to signage.rb

--- bin/signaged.rb
+++ bin/signaged.rb
@@ -1,5 +1,29 @@
+require_relative '../lib/synchronizer.rb'
 
-while true
-  sleep 1
-  puts "Hello"
-end
+usage = "Usage: signage.rb SERVER_URL [SERIAL]"
+
+default_serial = "CD9AK29HYCGPAH09"
+prod_server = "http://admin.tvopen.com.br"
+
+# The production server address
+SERVER = begin
+           if ARGV[0]
+             ARGV[0]
+           else
+             puts "error: Invalid server URL"
+             puts usage
+             exit 1
+           end
+         end
+
+# This machine's identification
+SERIAL = begin 
+           if ARGV[1]
+             ARGV[1]
+           else
+             exec "cat /proc/cpuinfo | grep Serial | cut -d' ' -f2"
+           end
+         end
+
+synchronizer = Synchronizer.new(SERVER, SERIAL)
+synchronizer.sync
---
+++ lib/synchronizer.rb
@@ -0,0 +1,86 @@
+#! /usr/bin/ruby
+
+require 'rubygems'
+require 'net/http'
+require 'json'
+
+content_dir = '/Users/felipe/code/signage/downloads'
+
+# Downloadable object
+class Loadable
+  def initialize(url)
+    @url = url
+  end
+
+  def download
+    unless File.exist?(file_path)
+      File.open(file_path, "wb") do |file|
+        file.write(response.body)
+      end
+    end
+  end
+
+  def response
+    Net::HTTP.get_response url
+  end
+
+  def filename
+    uri = URI.parse(url)
+    File.basename(uri.path)
+  end
+
+  def file_path
+    "#{content_dir}/#{@type}/#{@filename}"
+  end
+end
+
+# Videos can be downloaded
+class Video < Loadable
+  @type = "video"
+end
+
+# HTML articles can be downloaded
+class Article < Loadable
+  @type = "article"
+end
+
+class Synchronizer
+  attr_accessor :itineraries
+
+  def initialize(server, serial)
+    @server = server
+    @serial = serial
+  end
+
+  def itineraries_uri
+    URI "#{@server}/api/devices/#{@serial}.json"
+  end
+
+  def response
+    Net::HTTP.get_response itineraries_uri
+  end
+
+  # {
+  #   id: "f212512",
+  #   itineraries: [
+  #     {
+  #       type: "video" | "article" | ...,
+  #       url: "http://..."
+  #     },
+  #     ...
+  #   ]
+  # }
+  def json_response
+    JSON.parse response.body
+  end
+
+  def sync
+    puts itineraries_uri
+    json = json_response
+    json['itineraries'].each do |itinerary|
+      item = itinerary['type'] == 'video' ? Video.new(itinerary.url) : Article.new(itinerary.url)
+      item.download
+      @itineraries << item
+    end
+  end
+end
