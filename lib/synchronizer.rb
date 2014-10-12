c9f233c82be8753315c0381dca39542fd355d6e4
Matheus Oliveira <matheus.vetor@gmail.com>
2014-10-12 17:06:37 -0300

    added duplicity

--- lib/synchronizer.rb
+++ lib/synchronizer.rb
@@ -101,9 +101,11 @@ class Schedule
   def self.parse_itineraries(serialized_itineraries)
     parsed_itineraries = JSON.parse(serialized_itineraries)
     itineraries = []
+    article_duration = json['article_duration']
+    disable_audio = json['disable_audio']
     parsed_itineraries.each do |itinerary|
       url = itinerary['url']
-      item = itinerary['type'] == 'video' ? Video.new(url) : Article.new(url)
+      item = itinerary['type'] == 'video' ? Video.new(url, disable_audio) : Article.new(url, article_duration)
       itineraries << item
     end
     itineraries
