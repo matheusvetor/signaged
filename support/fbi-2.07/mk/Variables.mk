9d24b3a576f7fc34b9988ab6571a85977798562f
Felipe Oliveira Carvalho <felipekde@gmail.com>
2014-10-24 17:33:14 -0300

    Remove profiling flags

--- support/fbi-2.07/mk/Variables.mk
+++ support/fbi-2.07/mk/Variables.mk
@@ -36,7 +36,7 @@ INSTALL_DATA	:= $(INSTALL) -m 644
 INSTALL_DIR	:= $(INSTALL) -d
 
 # cflags
-CFLAGS  	?= $(shell dpkg-buildflags --get CFLAGS) -g -pg -O2
+CFLAGS  	?= $(shell dpkg-buildflags --get CFLAGS) -O2
 CXXFLAGS	?= $(CFLAGS)
 CFLAGS		+= -Wall -Wmissing-prototypes -Wstrict-prototypes \
 		   -Wpointer-arith -Wunused
@@ -49,7 +49,7 @@ ifneq ($(wildcard /usr/local/include/*.h),)
   LDFLAGS += -L/usr/local/$(LIB)
 endif
 
-LDFLAGS 	?= $(shell dpkg-buildflags --get LDFLAGS) -g -pg
+LDFLAGS 	?= $(shell dpkg-buildflags --get LDFLAGS)
 
 # fixup include path for $(srcdir) != "."
 ifneq ($(srcdir),.)
