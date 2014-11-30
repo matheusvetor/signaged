7e3e8ffd90c7da19c4eef6659dc60182d910472f
Matheus Oliveira <matheus.vetor@gmail.com>
2014-11-30 14:52:47 -0300

    removed broken code

--- bin/signaged_player.rb
+++ bin/signaged_player.rb
@@ -57,12 +57,6 @@ Signal.trap("TERM") do
   should_end = true
 end
 
-if comand_seq.empty?
-  exec "fbi -T 2 -a -noverbose #{base_dir}/../assets/images/no-content.png > /dev/null 2>&1"
-else
-  exec "fbi -T 2 -reset"
-end
-
 while !should_end
   command_seq.each do |it|
     case it.type
