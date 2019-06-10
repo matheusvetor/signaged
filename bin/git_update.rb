d310ea6f2a16e013692d75e89b056ad789ad70a5
Matheus Oliveira <matheus.vetor@gmail.com>
2019-06-10 10:18:11 -0300

    fix git script

--- bin/git_update.rb
+++ bin/git_update.rb
@@ -7,7 +7,8 @@ Dotenv.load('/home/pi/signaged/.env')
 logger = Logger.new('/home/pi/signaged/logs/git_update.log', 5, 100000)
 
 while true
-  command = "git -C /home/pi/signaged pull origin #{ENV['GIT_BRANCH']}"
+  command = "/usr/bin/git -C /home/pi/signaged pull origin #{ENV['GIT_BRANCH']}"
+  system(command)
   logger.info("Trying to get code updates: #{command}")
   sleep 300
 end
