dcec2e79c206257307cbafd7e2bfd0a185abe736
Matheus Oliveira <matheus.vetor@gmail.com>
2019-06-10 10:29:37 -0300

    Fix

--- bin/auth_unit.rb
+++ bin/auth_unit.rb
@@ -7,11 +7,11 @@ Dotenv.load('/home/pi/signaged/.env')
 logger = Logger.new('/home/pi/signaged/logs/auth_unit.log', 5, 100000)
 
 while true
-  if ENV['NEED_TO_CONNECT_UNIT_WIFI']
+  if ENV['CONNECT_UNIT_WIFI'].eql?('true')
     `/usr/local/bin/casperjs /home/pi/signaged/autologin/app.js`
     logger.info("Trying to connect into unit network")
   else
     logger.info("No need to connect into unit network")
   end
-  sleep 600
+  sleep 21600
 end
