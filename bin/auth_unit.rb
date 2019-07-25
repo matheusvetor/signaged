a10626e4d4fed3467fc3b9b521d8a8422f1e8541
Matheus Oliveira <matheus.vetor@gmail.com>
2019-07-25 16:59:29 -0300

    Added auth unit to the path

--- bin/auth_unit.rb
+++ bin/auth_unit.rb
@@ -8,7 +8,7 @@ logger = Logger.new('/home/pi/signaged/logs/auth_unit.log', 5, 100000)
 
 while true
   if ENV['CONNECT_UNIT_WIFI'].eql?('true')
-    `/usr/local/bin/casperjs /home/pi/signaged/autologin/app.js`
+    `/usr/local/bin/casperjs /home/pi/signaged/lib/auth_unit.js`
     logger.info("Trying to connect into unit network")
   else
     logger.info("No need to connect into unit network")
